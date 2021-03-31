a4efa5c2f6456f58b0c7843aec0c70742551b869
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-03-31 13:05:06 -0400

    Python Tutorial #3 - Simple Arithmetic
    Learn to do simple arithmetic!
    March 31st 2021
    13:05 UTC - 5

---
+++ Tut3-SimpleArithmetic.py
@@ -0,0 +1,24 @@
+x = 7
+y = 9
+# two random numbers that me likes
+
+print('Addition:\t\t', x, '+', y, '=', x + y)
+# add : x + y
+
+print('\nSubtraction:\t\t', x, '-', y , '=', x - y)
+# subtract : x - y
+
+print('\nMultiplication:\t\t', x, 'x', y, '=', x * y)
+# multiply : x * y
+
+print('\nDivision:\t\t', x, '÷', y , '=', x / y)
+# divide : x / y
+
+print('\nFloor Division:\t\t', x, '÷', y, '=', x // y)
+# floor divide (returns largest possible int "⌊ ⌋") : x // y
+
+print('\nRemainder\t\t', x, '%', y , '=', x % y)
+# the remainder : x % y
+ 
+print('\nExponent:\t\t', x, '² = ', x ** y)
+# exponents : x ** y
