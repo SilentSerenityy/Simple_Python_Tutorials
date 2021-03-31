e8f2fc885b55a56770f376a09634714c04322f2c
Shadi Alostaz <66328589+SilentSerenityy@users.noreply.github.com>
2021-03-31 09:55:10 -0400

    Python Tutorial #2 - Button Presses
    Color Switching Buttons!
    March 31st, 2021
    09:55 UTC - 5

---
+++ Tut2-ButtonPressing.py
@@ -0,0 +1,27 @@
+from tkinter import *
+window = Tk()
+# imports the package to show the window and defines it
+
+window.title('Color Switching!')
+# window title
+
+btn_end = Button(window , text = 'Exit' , command = exit)
+# the end button
+
+def tog() :
+	if window.cget('bg') == 'blue' :
+		window.configure(bg = 'red')
+	else :
+		window.configure(bg = 'blue')
+# the window will come blank, but if you press "Color Switch", it will turn blue
+
+# press it again, it turns red
+
+btn_tog = Button(window , text = 'Color Switch!' , command = tog)
+# press da button!
+
+btn_tog.pack(padx = 120 , pady = 20)
+btn_end.pack(padx = 120 , pady = 20)
+# defines the buttons measurments 
+
+window.mainloop()
